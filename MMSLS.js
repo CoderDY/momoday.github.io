c134501c25b33ae326f810755b49870bdbe90ac3
唯尔青山 <89464845+CoderDY@users.noreply.github.com>
2022-09-17 23:57:18 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -19,7 +19,7 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    "https://www.maimemo.com/share/page?uid=20934648&pid=341c6fcd3702d9c50d2bd979c2765a13&tid=2fe6da2cdc6fe317051fd29ea915f4e8",
+    "https://www.maimemo.com/share/page?uid=20934648&pid=3e54cb43654d77c1fff76a444a64514c&tid=b3e3a9d1fa0bd708197dd579c7644563",
   ]
   /**
    * 生成随机数字
